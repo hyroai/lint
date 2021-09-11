c1ae68d7fde5a4a0f8939a2fd20e2e77c6332f9f
Uri <uri.valevski@gmail.com>
2021-09-11 14:17:08 +0300

    bugfix

--- lint/make_api_doc.py
+++ lint/make_api_doc.py
@@ -2,70 +2,77 @@
 import importlib
 import inspect
 import sys
-from typing import Any, Text, Tuple
+from typing import Any, Callable, Iterable, Tuple
 
 import gamla
 
-_PACKAGE_NAME = sys.argv[1]
-package = importlib.import_module(_PACKAGE_NAME)
+
+def _module_filter(package_name: str):
+    def module_filter(module):
+        return (
+            inspect.ismodule(module)
+            and package_name in str(module)
+            and "test" not in str(module)
+        )
+
+    return module_filter
 
 
-def _module_filter(module):
-    return (
-        inspect.ismodule(module)
-        and _PACKAGE_NAME in str(module)
-        and "test" not in str(module)
-    )
-
-
-def _get_modules() -> Tuple[Tuple[Text, Any], ...]:
-    return tuple(inspect.getmembers(package, _module_filter))
-
-
-def _get_function_table_entries(module: Tuple[Text, Any]) -> Text:
+def _get_function_table_entries(module: Tuple[str, Any]) -> str:
     return "".join(
-        [
-            f"   {o[0]}\n"
-            for o in inspect.getmembers(module)
-            if inspect.isfunction(o[1]) and o[0][0] != "_"
-        ],
+        f"   {o[0]}\n"
+        for o in inspect.getmembers(module)
+        if inspect.isfunction(o[1]) and o[0][0] != "_"
     )
 
 
-def _concat_module_table_string(string_so_far: Text, module: Tuple[Text, Any]) -> Text:
-    return "".join(
-        gamla.concat_with(
-            f"{module[0]}\n{len(module[0]) * '-'}\n\n.. currentmodule:: {_PACKAGE_NAME}.{module[0]}\n\n.. autosummary::\n{_get_function_table_entries(module[1])}\n",
-            string_so_far,
-        ),
-    )
+def _concat_module_table_string(package_name: str):
+    def concat_module_table_string(string_so_far: str, module: Tuple[str, Any]) -> str:
+        return "".join(
+            gamla.concat_with(
+                f"{module[0]}\n{len(module[0]) * '-'}\n\n.. currentmodule:: {package_name}.{module[0]}\n\n.. autosummary::\n{_get_function_table_entries(module[1])}\n",
+                string_so_far,
+            ),
+        )
+
+    return concat_module_table_string
 
 
-def _concat_module_members_string(
-    string_so_far: Text,
-    module: Tuple[Text, Any],
-) -> Text:
-    return "".join(
-        gamla.concat_with(
-            f".. automodule:: {_PACKAGE_NAME}.{module[0]}\n   :members:\n\n",
-            string_so_far,
-        ),
-    )
+def _concat_module_members_string(package_name: str):
+    def concat_module_members_string(string_so_far: str, module: str) -> str:
+        return "".join(
+            gamla.concat_with(
+                f".. automodule:: {package_name}.{module}\n   :members:\n\n",
+                string_so_far,
+            ),
+        )
+
+    return concat_module_members_string
 
 
-def _create_api_string(modules: Tuple[Tuple[Text, Any], ...]) -> Text:
-    return gamla.reduce(
-        _concat_module_members_string,
-        gamla.reduce(_concat_module_table_string, "API\n===\n\n", modules)
+def _create_api_string(package_name: str) -> Callable[[Iterable[str]], str]:
+    return lambda modules: gamla.reduce(
+        _concat_module_members_string(package_name),
+        gamla.reduce(_concat_module_table_string(package_name), "API\n===\n\n", modules)
         + "Definitions\n-----------\n\n",
-        modules,
     )
 
 
 def main():
-    new_api = open("./docs/source/api.rst", "w")
-    new_api.write(_create_api_string(_get_modules()))
-    new_api.close()
+    raise Exception
+    gamla.pipe(
+        sys.argv[1],
+        inspect.getmembers,
+        gamla.filter(
+            gamla.compose_left(
+                gamla.second,
+                _module_filter(importlib.import_module(sys.argv[1])),
+            ),
+        ),
+        gamla.map(gamla.head),
+        gamla.prepare_and_apply(_create_api_string(sys.argv[1])),
+        open("./docs/source/api.rst", "w").write,
+    )
     return 0
 
 
