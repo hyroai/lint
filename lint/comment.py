192ee69ef430a37689347801ac3c3fbfc2c43dd7
amnon-hyro.ai <amnon@hyro.ai>
2025-03-24 10:23:16 +0200

    delete the bifurcate

--- lint/comment.py
+++ lint/comment.py
@@ -7,17 +7,15 @@ detect = gamla.compose_left(
     gamla.filter(gamla.compose_left(str.lower, lambda s: re.match(r"\s*(#|//).*", s))),
     gamla.remove(lambda s: re.search(r"# type: ignore", s)),
     gamla.remove(lambda s: re.search(r"noqa", s)),
-    gamla.bifurcate(
-        gamla.compose_left(
-            gamla.filter(
-                gamla.alljuxt(
-                    gamla.compose_left(str.lower, lambda s: re.search(r"\btodo\b", s)),
-                    lambda s: not re.search(r"(#|//)\sTODO\([a-zA-Z]+\):\s.*", s),
-                ),
+    gamla.compose_left(
+        gamla.filter(
+            gamla.alljuxt(
+                gamla.compose_left(str.lower, lambda s: re.search(r"\btodo\b", s)),
+                lambda s: not re.search(r"(#|//)\sTODO\([a-zA-Z]+\):\s.*", s),
             ),
-            gamla.map(
-                lambda s: f"Malformatted `TODO` syntax (should be `TODO(name): ...`): [{s}]",
-            ),
+        ),
+        gamla.map(
+            lambda s: f"Malformatted `TODO` syntax (should be `TODO(name): ...`): [{s}]",
         ),
     ),
     gamla.concat,
